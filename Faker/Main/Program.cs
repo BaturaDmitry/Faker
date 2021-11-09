0c57b9cc678fa19276725a3394db5eefb2840c4e
BaturaDmitry <koonnot@mail.ru>
2021-11-09 11:25:08 +0300

    Created byte and char generators

---
+++ Faker/Main/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Faker;
+using Faker.BaseTypes;
+
+namespace Faker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IGenerator i = new ByteGenerator();
+            Console.WriteLine(i.GeneratorType);
+        }
+    }
+}
