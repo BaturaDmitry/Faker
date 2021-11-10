55e265401d456d2c21bb011496bb8e850e13d7e9
BaturaDmitry <koonnot@mail.ru>
2021-11-10 08:07:05 +0300

    created two test classes

---
+++ Faker/Main/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Main
+{
+    public class User
+    {
+        public String name;
+        public int age;
+        public long test { get; set; }
+        public float money = 10.5f;
+        public  Dog[] dogs;
+        public Profile profile;
+    }
+
+   public class Dog
+    {
+        private string name; 
+        private User owner;
+        private Dog(string name,User owner)
+        {
+            this.name = name;
+            this.owner = owner;
+        }
+
+
+    }
+
+   public class Profile
+    {
+        public string address;
+        
+        public Profile()
+        {
+            this.address = address;
+        }
+        
+        
+    }
+}
